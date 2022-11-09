092ee5bd6511778cb925dd36efc40af90dabee33
Krishna Kanani <kananikrishna427@gmail.com>
2022-11-09 08:44:27 +0530

    Project initial setup

---
+++ src/testscripts/LoginTest.java
@@ -0,0 +1,28 @@
+package testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.PredefinedActions;
+import pages.LoginPage;
+
+public class LoginTest {
+
+	@Test
+	public void tc1() {
+		System.out.println("STEP - Launch Chrome Browser & Hit url");
+		PredefinedActions.start("https://harshaug2022-trials76.orangehrmlive.com");
+		
+		System.out.println("STEP - Enter valid login credentials");
+		LoginPage loginPage = new LoginPage();
+		loginPage.login("admin", "MPCs0K@ce1");
+		
+		System.out.println("VERIFY - home page is displayed");
+		String expetedTitle = "Employee Management";
+		String actualTitle = loginPage.getPageTitle();
+		
+		Assert.assertEquals(actualTitle, expetedTitle, "Expected title was " + expetedTitle + " but actual title was " + actualTitle);
+		
+		PredefinedActions.closeBrowser();
+	}
+}
